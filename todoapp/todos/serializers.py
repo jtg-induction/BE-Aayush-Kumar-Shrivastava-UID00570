0c2fbcefe259bbb4f16e5ddad37ccf67a8caf41f
Aayush Kumar Shrivastava <aayush.shrivastava@joshtechnologygroup.com>
2025-02-04 11:12:41 +0530

    Implement TodoViewSet Exercise8

--- todoapp/todos/serializers.py
+++ todoapp/todos/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+
 from .models import Todo
+from users.models import CustomUser
 
 
 class TodoSerializer(serializers.ModelSerializer):
@@ -32,10 +34,33 @@ class TodosWithinDateRangeSerializer(serializers.ModelSerializer):
 
     def get_status(self, obj):
         return "Done" if obj.done else "To Do"
-    
+
     def get_creator(self, obj):
         return obj.user.first_name + " " + obj.user.last_name
 
     class Meta:
         model = Todo
         fields = ['id', 'name', 'creator', 'email', 'created_at', 'status']
+
+
+class TodoAPICreateSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(read_only=True)
+    done = serializers.BooleanField(read_only=True)
+    date_created = serializers.DateTimeField(read_only=True)
+
+    todo = serializers.CharField(source='name', write_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(),
+                                                 source='user', write_only=True)
+
+    class Meta:
+        model = Todo
+        fields = ['user_id', 'todo', 'name', 'done', 'date_created']
+
+
+class TodoAPIResponseSerializer(serializers.ModelSerializer):
+    todo_id = serializers.IntegerField(source='id')
+    todo = serializers.CharField(source='name')
+
+    class Meta:
+        model = Todo
+        fields = ['todo_id', 'todo', 'done']
--- todoapp/todos/views.py
+++ todoapp/todos/views.py
@@ -1,25 +1,44 @@
-from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q  
 
+from rest_framework.viewsets import ModelViewSet 
+
+from .serializers import TodoAPICreateSerializer, TodoAPIResponseSerializer
+from .models import Todo
 
 class TodoAPIViewSet(ModelViewSet):
     """
-        success response for create/update/get
-        {
-          "name": "",
-          "done": true/false,
-          "date_created": ""
-        }
+       success response for create/update/get
+       {
+         "name": "",
+         "done": true/false,
+         "date_created": ""
+       }
 
-        success response for list
-        [
-          {
-            "name": "",
-            "done": true/false,
-            "date_created": ""
-          }
-        ]
+
+       success response for list
+       [
+         {
+           "name": "",
+           "done": true/false,
+           "date_created": ""
+         }
+       ]
     """
 
+    queryset = Todo.objects.all()
+    serializer_class = TodoAPICreateSerializer
 
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user_id', None)
+        queryset = None
+        if user_id is not None:
+            queryset = Todo.objects.filter(user__id=user_id)
+        else:
+            queryset = Todo.objects.all()
 
+        return queryset
 
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return TodoAPICreateSerializer
+        return TodoAPIResponseSerializer
